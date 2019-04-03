f61e287216027cf985911fa6432b5c72da0bd6c4
Landon Patmore <landon.patmore@gmail.com>
2019-04-03 03:44:44 -0400

    removed extras

--- assets/js/test.js
+++ assets/js/test.js
@@ -2,29 +2,28 @@
 
   var chart = new CanvasJS.Chart("heightVsSpeedChart", {
     axisY:[{
-      title: "Order",
+      title: "IAS",
       lineColor: "#C24642",
       tickColor: "#C24642",
       labelFontColor: "#C24642",
       titleFontColor: "#C24642",
-      suffix: "k"
+      suffix: "km/h"
     },
     {
-      title: "Footfall",
+      title: "TAS",
       lineColor: "#369EAD",
       tickColor: "#369EAD",
       labelFontColor: "#369EAD",
       titleFontColor: "#369EAD",
-      suffix: "k"
+      suffix: "km/h"
     }],
     axisY2: {
-      title: "Revenue",
+      title: "Height",
       lineColor: "#7F6084",
       tickColor: "#7F6084",
       labelFontColor: "#7F6084",
       titleFontColor: "#7F6084",
-      prefix: "$",
-      suffix: "k"
+      suffix: "m"
     },
     toolTip: {
       shared: true
@@ -34,69 +33,48 @@
     },
     data: [{
       type: "line",
-      name: "Footfall",
+      name: "IAS",
       color: "#369EAD",
       showInLegend: true,
       axisYIndex: 1,
-      dataPoints: [
-        { x: new Date(2017, 00, 7), y: 85.4 }, 
-        { x: new Date(2017, 00, 14), y: 92.7 },
-        { x: new Date(2017, 00, 21), y: 64.9 },
-        { x: new Date(2017, 00, 28), y: 58.0 },
-        { x: new Date(2017, 01, 4), y: 63.4 },
-        { x: new Date(2017, 01, 11), y: 69.9 },
-        { x: new Date(2017, 01, 18), y: 88.9 },
-        { x: new Date(2017, 01, 25), y: 66.3 },
-        { x: new Date(2017, 02, 4), y: 82.7 },
-        { x: new Date(2017, 02, 11), y: 60.2 },
-        { x: new Date(2017, 02, 18), y: 87.3 },
-        { x: new Date(2017, 02, 25), y: 98.5 }
-      ]
+      dataPoints: []
     },
     {
       type: "line",
-      name: "Order",
+      name: "TAS",
       color: "#C24642",
       axisYIndex: 0,
       showInLegend: true,
-      dataPoints: [
-        { x: new Date(2017, 00, 7), y: 32.3 }, 
-        { x: new Date(2017, 00, 14), y: 33.9 },
-        { x: new Date(2017, 00, 21), y: 26.0 },
-        { x: new Date(2017, 00, 28), y: 15.8 },
-        { x: new Date(2017, 01, 4), y: 18.6 },
-        { x: new Date(2017, 01, 11), y: 34.6 },
-        { x: new Date(2017, 01, 18), y: 37.7 },
-        { x: new Date(2017, 01, 25), y: 24.7 },
-        { x: new Date(2017, 02, 4), y: 35.9 },
-        { x: new Date(2017, 02, 11), y: 12.8 },
-        { x: new Date(2017, 02, 18), y: 38.1 },
-        { x: new Date(2017, 02, 25), y: 42.4 }
-      ]
+      dataPoints: []
     },
     {
       type: "line",
-      name: "Revenue",
+      name: "Height",
       color: "#7F6084",
       axisYType: "secondary",
       showInLegend: true,
-      dataPoints: [
-        { x: new Date(2017, 00, 7), y: 42.5 }, 
-        { x: new Date(2017, 00, 14), y: 44.3 },
-        { x: new Date(2017, 00, 21), y: 28.7 },
-        { x: new Date(2017, 00, 28), y: 22.5 },
-        { x: new Date(2017, 01, 4), y: 25.6 },
-        { x: new Date(2017, 01, 11), y: 45.7 },
-        { x: new Date(2017, 01, 18), y: 54.6 },
-        { x: new Date(2017, 01, 25), y: 32.0 },
-        { x: new Date(2017, 02, 4), y: 43.9 },
-        { x: new Date(2017, 02, 11), y: 26.4 },
-        { x: new Date(2017, 02, 18), y: 40.3 },
-        { x: new Date(2017, 02, 25), y: 54.2 }
-      ]
+      dataPoints: []
     }]
   });
   chart.render();
+
+  var i = 0;
+  function updateChart(data) {
+    chart.data.forEach((d) => {
+      if (i > 200) {
+        d.dataPoints.shift()
+      }
+      if (d.name == "IAS") {
+        d.dataPoints.push({x: i, y: data["IAS, km/h"]})
+      } else if (d.name == "TAS") {
+        d.dataPoints.push({x: i, y:data["TAS, km/h"]})
+      } else if (d.name == "Height") {
+        d.dataPoints.push({x: i, y:data["H, m"]})
+      }
+    })
+    i++;
+    chart.render();
+  }
 
   setInterval(requestData, 100);
   function requestData() {
@@ -107,6 +85,8 @@
       updateElement($("#tas"), data["TAS, km/h"], null, null, null)
       updateElement($("#rpm"), data["RPM 1"], null, null, null)
       updateElement($("#geforce"), data["Ny"], null, null, null)
+
+      updateChart(data);
     })
 
     $.getJSON("http://localhost:7000/indicators", function (data) {
